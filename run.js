f2dc0df9da3c35f8ea8178af67cab2a8831dd3e6
Andrey Savelev <savelevcorr@yandex.ru>
2017-10-13 21:53:32 +0300

    Update run.js

--- run.js
+++ run.js
@@ -3,5 +3,4 @@ const chalk = require('chalk');
 const figlet = require('figlet');
 const inquirer = require('inquirer');
 const yargs = require('yargs');
-const { exec } = require('child_process');
-const { fork } = require('child_process');
+const { exec, fork } = require('child_process');
