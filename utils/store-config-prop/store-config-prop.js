89cac0454a28d12ef8ea9d51f42e006c0f3d7f58
Andrey Savelev <savelevcorr@yandex.ru>
2017-10-30 00:32:46 +0300

    Fix eslint errors

--- utils/store-config-prop/store-config-prop.js
+++ utils/store-config-prop/store-config-prop.js
@@ -4,30 +4,30 @@ const FS = require('fs');
  * Store property key/value pair in the special ui5.config.json
  * @prop {string} propKey - Name of property
  * @prop {string} propValue - Value of property
- * @return {Promise} Promise 
+ * @return {Promise} Promise
  */
 exports.storeConfigProp = (propKey, propValue) => {
-    const CONFIG_FILE_PATH = './ui5.config.json';
-    let propertyPair = {};
-    let existPairs;
+  const CONFIG_FILE_PATH = './ui5.config.json';
+  const propertyPair = {};
+  let existPairs;
 
-    propertyPair[propKey] = propValue;
-    
-    // if file does not exist, create it
-    return new Promise((resolve) => {
-        FS.access(CONFIG_FILE_PATH, (error) => {
-            if (error) {
-                FS.writeFileSync(
-                    CONFIG_FILE_PATH, 
-                    JSON.stringify(propertyPair)
-                );
-            } else {
-                existPairs = FS.readFileSync(CONFIG_FILE_PATH);
-                existPairs[propKey] = propertyPair;
-    
-                FS.writeFileSync(CONFIG_FILE_PATH, existPairs);
-            }
-            resolve();
-        });
+  propertyPair[propKey] = propValue;
+
+  // if file does not exist, create it
+  return new Promise((resolve) => {
+    FS.access(CONFIG_FILE_PATH, (error) => {
+      if (error) {
+        FS.writeFileSync(
+          CONFIG_FILE_PATH,
+          JSON.stringify(propertyPair)
+        );
+      } else {
+        existPairs = FS.readFileSync(CONFIG_FILE_PATH);
+        existPairs[propKey] = propertyPair;
+
+        FS.writeFileSync(CONFIG_FILE_PATH, existPairs);
+      }
+      resolve();
     });
+  });
 };
--- utils/store-config-prop/store-config-prop.spec.js
+++ utils/store-config-prop/store-config-prop.spec.js
@@ -1,29 +1,26 @@
 const fs = require('fs');
-const path = require('path');
 const { storeConfigProp } = require('./store-config-prop');
 
 const { expect } = require('chai');
 
 describe('storeConfigProp()', () => {
+  it('Should store config props pair', (done) => {
+    const PROP_KEY = 'propKey';
+    const PROP_VALUE = 'propValue';
+    let storedProps;
 
-    it('Should store config props pair', (done) => {
-        const PROP_KEY = 'propKey';
-        const PROP_VALUE = 'propValue';
-        let storedProps;
+    Promise.resolve()
+      .then(() => storeConfigProp(PROP_KEY, PROP_VALUE))
+      .then(() => {
+        storedProps = fs.readFileSync('./ui5.config.json', 'utf8');
+        storedProps = JSON.parse(storedProps);
 
-        Promise.resolve()
-            .then(() => {
-                return storeConfigProp(PROP_KEY, PROP_VALUE);
-            })
-            .then(() => {
-                storedProps = fs.readFileSync('./ui5.config.json', 'utf8');
-                storedProps = JSON.parse(storedProps);
-        
-                expect(storedProps[PROP_KEY]).eq(PROP_VALUE);
-            })
-            .then(() => {
-                done();
-            })
-            .catch(e => console.error(e));
-    })
+        expect(storedProps[PROP_KEY]).eq(PROP_VALUE);
+      })
+      .then(() => {
+        done();
+      })
+      /* eslint no-console: 0 */
+      .catch(e => console.error(e));
+  });
 });
